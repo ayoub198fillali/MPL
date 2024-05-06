91a5cbf3ac7e23054184abc019b42f9241e16390
ayoub198fillali <ayoub.fillali@usmba.ac.ma>
2024-05-06 21:44:40 +0200

    FIrst Version

---
+++ assets/js/twoImg.js
@@ -0,0 +1,31 @@
+$("#slider1").on("input change", (e) => {
+  const sliderPos = e.target.value;
+  // Update the width of the foreground image
+  $("#foreground-img1").css("width", `calc(${sliderPos}% - 4px)`);
+  // Update the position of the slider button
+  $("#slider-button1").css("left", `calc(${sliderPos}% - 18px)`);
+});
+
+$("#slider2").on("input change", (e) => {
+  const sliderPos = e.target.value;
+  // Update the width of the foreground image
+  $("#foreground-img2").css("width", `calc(${sliderPos}% - 4px)`);
+  // Update the position of the slider button
+  $("#slider-button2").css("left", `calc(${sliderPos}% - 18px)`);
+});
+
+$("#slider3").on("input change", (e) => {
+  const sliderPos = e.target.value;
+  // Update the width of the foreground image
+  $("#foreground-img3").css("width", `calc(${sliderPos}% - 4px)`);
+  // Update the position of the slider button
+  $("#slider-button3").css("left", `calc(${sliderPos}% - 18px)`);
+});
+
+$("#slider4").on("input change", (e) => {
+  const sliderPos = e.target.value;
+  // Update the width of the foreground image
+  $("#foreground-img4").css("width", `calc(${sliderPos}% - 4px)`);
+  // Update the position of the slider button
+  $("#slider-button4").css("left", `calc(${sliderPos}% - 18px)`);
+});
